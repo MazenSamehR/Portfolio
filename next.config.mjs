9295904bab5593f9828851a72548e09dab04f60f
MazenSamehR <samehmazen868@gmail.com>
2025-07-09 23:15:59 +0300

    deploy v1

--- next.config.mjs
+++ next.config.mjs
@@ -1,40 +1,45 @@
-import {withSentryConfig} from '@sentry/nextjs';
+import { withSentryConfig } from "@sentry/nextjs";
+import { PHASE_EXPORT } from "next/constants";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const baseConfig = {
+  images: {
+    domains: ["your-image-domains.com"], // Add any domains if needed
+  },
+  reactStrictMode: true,
+};
 
-export default withSentryConfig(nextConfig, {
-// For all available options, see:
-// https://github.com/getsentry/sentry-webpack-plugin#options
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (phase) => {
+  const isStaticExport = phase === PHASE_EXPORT;
 
-// Suppresses source map uploading logs during build
-silent: true,
-org: "javascript-mastery",
-project: "javascript-nextjs",
-}, {
-// For all available options, see:
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+  // For GitHub Pages (static export)
+  if (isStaticExport) {
+    return {
+      ...baseConfig,
+      output: "export",
+      images: {
+        unoptimized: true,
+      },
+      basePath: "/your-repo-name",
+      assetPrefix: "/your-repo-name/",
+    };
+  }
 
-// Upload a larger set of source maps for prettier stack traces (increases build time)
-widenClientFileUpload: true,
-
-// Transpiles SDK to be compatible with IE11 (increases bundle size)
-transpileClientSDK: true,
-
-// Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-// This can increase your server load as well as your hosting bill.
-// Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-// side errors will fail.
-// tunnelRoute: "/monitoring",
-
-// Hides source maps from generated client bundles
-hideSourceMaps: true,
-
-// Automatically tree-shake Sentry logger statements to reduce bundle size
-disableLogger: true,
-
-// Enables automatic instrumentation of Vercel Cron Monitors.
-// See the following for more information:
-// https://docs.sentry.io/product/crons/
-// https://vercel.com/docs/cron-jobs
-automaticVercelMonitors: true,
-});
+  // For Vercel (dynamic build with Sentry support)
+  return withSentryConfig(
+    baseConfig,
+    {
+      silent: true,
+      org: "javascript-mastery",
+      project: "javascript-nextjs",
+    },
+    {
+      widenClientFileUpload: true,
+      transpileClientSDK: true,
+      hideSourceMaps: true,
+      disableLogger: true,
+      automaticVercelMonitors: true,
+    }
+  );
+};
